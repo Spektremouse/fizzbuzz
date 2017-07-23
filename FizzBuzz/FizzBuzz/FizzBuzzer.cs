0106b7d6989b05cfedf3d721e89016a2a044bfb3
spektremouse <tompachico@gmail.com>
2017-07-23 16:52:50 +0100

    FizzBuzz implementation and Unit Tests.

---
+++ FizzBuzz/FizzBuzz/FizzBuzzer.cs
@@ -0,0 +1,134 @@
+﻿using System;
+
+namespace FizzBuzz
+{
+    public class FizzBuzzer
+    {
+        private int mLength;
+        private int mFizzMultiple = 3;
+        private int mBuzzMultiple = 5;        
+        private const string mFizz = "Fizz";
+        private const string mBuzz = "Buzz";       
+        private const string mFizzBuzz = "FizzBuzz";
+        private FizzBuzzer() { }
+
+        /// <summary>
+        /// Constructor for a FizzBuzz instance.
+        /// </summary>
+        /// <param name="length"></param>
+        public FizzBuzzer(int length = 100)
+        {
+            mLength = length;
+        }
+
+        /// <summary>
+        /// The number to which FizzBuzz will loop.
+        /// </summary>
+        public int Length
+        {
+            get { return mLength; }
+            set { mLength = value; }
+        }
+
+        /// <summary>
+        /// The multiple to be used for Fizz.
+        /// </summary>
+        public int FizzMultiple
+        {
+            get { return mFizzMultiple; }
+            set { mFizzMultiple = value; }
+        }
+
+        /// <summary>
+        /// The multiple to be used for Buzz.
+        /// </summary>
+        public int BuzzMultiple
+        {
+            get { return mBuzzMultiple; }
+            set { mBuzzMultiple = value; }
+        }
+
+        /// <summary>
+        /// Checks that an integer is a FizzMultiple.
+        /// </summary>
+        /// <param name="number">The integer to check.</param>
+        /// <returns>True when the integer is a FizzMultiple</returns>
+        public bool IsFizz(int number)
+        {
+            if (number % mFizzMultiple == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks that an integer is a BuzzMultiple.
+        /// </summary>
+        /// <param name="number">The integer to check.</param>
+        /// <returns>True when the integer is a BuzzMultiple</returns>
+        public bool IsBuzz(int number)
+        {
+            if (number % mBuzzMultiple == 0)
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Checks that an integer is a FizzMultiple and a BuzzMultiple.
+        /// </summary>
+        /// <param name="number">The integer to check.</param>
+        /// <returns>True when the integer is a FizzMultiple and a BuzzMultiple</returns>
+        public bool IsFizzBuzz(int number)
+        {
+            if (IsFizz(number) && IsBuzz(number))
+            {
+                return true;
+            }
+            return false;
+        }
+
+        /// <summary>
+        /// Resets the Length, Fizz and Buzz multiples to default values.
+        /// </summary>
+        public void Reset()
+        {
+            FizzMultiple = 3;
+            BuzzMultiple = 5;
+            Length = 100;
+        }
+
+        static void Main(string[] args)
+        {
+            FizzBuzzer fb = new FizzBuzzer();
+            //Can be further customized for more FizzBuzz Results!
+            //fb.Length = 200;
+            //fb.FizzMultiple = 4;
+            //fb.BuzzMultiple = 10;
+
+            for (int i = 1; i <= fb.Length; i++)
+            {
+                if (fb.IsFizzBuzz(i))
+                {
+                    Console.WriteLine("FizzBuzz");
+                }
+                else if (fb.IsBuzz(i))
+                {
+                    Console.WriteLine("Buzz");
+                }
+                else if (fb.IsFizz(i))
+                {
+                    Console.WriteLine("Fizz");
+                }
+                else
+                {
+                    Console.WriteLine(i);
+                }
+            }
+            Console.WriteLine("Finished. Press any key to exit...");
+            Console.ReadKey();
+        }
+    }
+}
---
+++ FizzBuzz/FizzBuzzTests/FizzBuzzTest.cs
@@ -0,0 +1,114 @@
+﻿using FizzBuzz;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using System;
+
+namespace FizzBuzzTests
+{
+    [TestClass]
+    public class FizzBuzzTest
+    {
+        [TestMethod]
+        public void When_NotMultipleOf3_Expect_IsFizzAsFalse()
+        {
+            //Arrange   
+            var fb = new FizzBuzzer();
+            fb.FizzMultiple = 3;
+            const int expected = 8;
+            //Act
+            bool result = fb.IsFizz(expected);
+            //Assert
+            Assert.AreEqual(false, result);         
+        }
+
+        [TestMethod]
+        public void When_MultipleOf3_Expect_IsFizzAsTrue()
+        {
+            //Arrange   
+            var fb = new FizzBuzzer();
+            fb.FizzMultiple = 3;
+            const int expected = 9;
+            //Act
+            bool result = fb.IsFizz(expected);
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void When_MultipleOf5_Expect_IsBuzzAsTrue()
+        {
+            //Arrange   
+            var fb = new FizzBuzzer();
+            fb.BuzzMultiple = 5;
+            const int expected = 10;
+            //Act
+            bool result = fb.IsBuzz(expected);
+            //Assert
+            Assert.AreEqual(true, result);
+        }
+
+        [TestMethod]
+        public void When_NotMultipleOf5_Expect_IsBuzzAsFalse()
+        {
+            //Arrange   
+            var fb = new FizzBuzzer();
+            fb.BuzzMultiple = 5;
+            const int expected = 11;
+            //Act
+            bool result = fb.IsBuzz(expected);
+            //Assert
+            Assert.AreEqual(false, result);
+        }
+
+        [TestMethod]
+        public void Should_ResetVariables_ToDefaultValues()
+        {
+            //Arrange   
+            var fb = new FizzBuzzer();
+            fb.FizzMultiple = 9;
+            fb.BuzzMultiple = 10;
+            fb.Length = 1220;
+            const int expectedFizzMultiple = 3;
+            const int expectedBuzzMultiple = 5;
+            const int expectedLength = 100;
+            //Act
+            fb.Reset();
+            //Assert
+            Assert.AreEqual(expectedFizzMultiple, fb.FizzMultiple);
+            Assert.AreEqual(expectedBuzzMultiple, fb.BuzzMultiple);
+            Assert.AreEqual(expectedLength, fb.Length);
+        }
+
+        [TestMethod]
+        public void Should_GetAndSet_Length()
+        {
+            //Arrange
+            int expected = new Random().Next();
+            //Act
+            var fb = new FizzBuzzer() { Length = expected };
+            //Assert
+            Assert.AreEqual(expected, fb.Length);
+        }
+
+        [TestMethod]
+        public void Should_GetAndSet_FizzMultiple()
+        {
+            //Arrange
+            int expected = new Random().Next();
+            //Act
+            var fb = new FizzBuzzer() { FizzMultiple = expected };
+            //Assert
+            Assert.AreEqual(expected, fb.FizzMultiple);
+        }
+
+        [TestMethod]
+        public void Should_GetAndSet_BuzzMultiple()
+        {
+            //Arrange
+            int expected = new Random().Next();
+            //Act
+            var fb = new FizzBuzzer() { BuzzMultiple = expected };
+            //Assert
+            Assert.AreEqual(expected, fb.BuzzMultiple);
+        }
+    }
+}
